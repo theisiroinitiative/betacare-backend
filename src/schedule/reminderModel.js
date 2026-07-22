923724ebd2dded5ef47b82e43f166509e3c38767
theisiroinitiative <the.isiro.initiative@gmail.com>
2026-07-22 08:12:19 +0100

    Fix import path for dbConfig in reminderModel.js

--- src/schedule/reminderModel.js
+++ src/schedule/reminderModel.js
@@ -1,5 +1,5 @@
 import { DataTypes } from 'sequelize';
-import sequelize from '../../config/dbConfig.js';
+import sequelize from '../config/dbConfig.js';
 
 const Reminder = sequelize.define('Reminder', {
     id: {
